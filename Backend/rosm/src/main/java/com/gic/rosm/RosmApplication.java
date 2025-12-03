fe4969124ebe15bb870a891d5e40ec6c72a9f521
zinlinhtet-gic <zinlinhtet@gicjp.com>
2025-12-03 13:34:09 +0630

    initial commit

---
+++ Backend/rosm/src/main/java/com/gic/rosm/RosmApplication.java
@@ -0,0 +1,13 @@
+package com.gic.rosm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RosmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RosmApplication.class, args);
+	}
+
+}
---
+++ Backend/rosm/src/test/java/com/gic/rosm/RosmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gic.rosm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RosmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

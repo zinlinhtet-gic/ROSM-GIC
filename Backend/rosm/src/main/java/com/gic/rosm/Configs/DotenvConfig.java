92a31bb417b210b4d7d35e6f65c7ae60763f7f95
zinlinhtet-gic <zinlinhtet@gicjp.com>
2025-12-04 14:38:55 +0630

    Added Docker but still having error

--- Backend/rosm/src/main/java/com/gic/rosm/Configs/DotenvConfig.java
+++ Backend/rosm/src/main/java/com/gic/rosm/Configs/DotenvConfig.java
@@ -1,27 +1,26 @@
 package com.gic.rosm.Configs;
 
-import com.mongodb.ConnectionString;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import io.github.cdimascio.dotenv.Dotenv;
+import javax.sql.DataSource;
+
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @Configuration
 public class DotenvConfig {
 
     @Bean(name = "mainDataSource")
     public DataSource mainDataSource() {
-        Dotenv dotenv = Dotenv.load();
+        Dotenv dotenv = Dotenv.configure().directory("./").load();
 
         String dbUrl = dotenv.get("DB_URL");
         String dbUsername = dotenv.get("DB_USERNAME");
         String dbPassword = dotenv.get("DB_PASSWORD");
-
+        System.out.println("DB_URL: " + dbUrl);
+        System.out.println("DB_USERNAME: " + dbUsername);
+        System.out.println("DB_PASSWORD: " + dbPassword);
         return DataSourceBuilder.create()
                 .url(dbUrl)
                 .username(dbUsername)
@@ -30,19 +29,19 @@ public class DotenvConfig {
                 .build();
     }
 
-    @Bean(name = "mongoDataSource")
-    public MongoClient mongoDataSource() {
-        Dotenv dotenv = Dotenv.load();
+    // @Bean(name = "mongoDataSource")
+    // public MongoClient mongoDataSource() {
+    //     Dotenv dotenv = Dotenv.load();
 
-        String dbUrl = dotenv.get("MONGO_URL");        // example: mongodb://localhost:27017
-        String dbName = dotenv.get("MONGO_DBNAME");    // example: restaurant
+    //     String dbUrl = dotenv.get("MONGO_URL");        // example: mongodb://localhost:27017
+    //     String dbName = dotenv.get("MONGO_DBNAME");    // example: restaurant
 
-        ConnectionString connectionString = new ConnectionString(dbUrl + "/" + dbName);
+    //     ConnectionString connectionString = new ConnectionString(dbUrl + "/" + dbName);
 
-        MongoClientSettings settings = MongoClientSettings.builder()
-                .applyConnectionString(connectionString)
-                .build();
+    //     MongoClientSettings settings = MongoClientSettings.builder()
+    //             .applyConnectionString(connectionString)
+    //             .build();
 
-        return MongoClients.create(settings);
-    }
+    //     return MongoClients.create(settings);
+    // }
 }
